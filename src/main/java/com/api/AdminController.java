3d989f57c959df3c1ab0c8b78177b24ba215730d
Rafael Henriques <rfchenriques@gmail.com>
2018-12-08 14:12:17 +0000

    Methods Defined

--- src/main/java/com/api/AdminController.java
+++ src/main/java/com/api/AdminController.java
@@ -1,11 +1,14 @@
 package com.api;
 
 import com.api.data.Item;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @RestController
@@ -19,15 +22,39 @@ public class AdminController {
 
     // Purchases Topic
     @RequestMapping(value = "/units-sold-each-item", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Item> soldUnitsOfEachItem(@RequestParam Integer minutes){
+    public List<Item> soldUnitsOfEachItem(@NotNull @RequestParam Integer minutes){
         return null;
     }
 
     // Purchases Topic
     @RequestMapping(value = "/maximum-price-of-each-item-sold", produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Item> maximumPriceOfEachItemSold(@RequestParam Integer minutes){
+    public List<Item> maximumPriceOfEachItemSold(@NotNull @RequestParam Integer minutes){
         return null;
     }
 
+    // Shipments Topic
+    @RequestMapping(value = "/average-number-shipments-each-item-sold", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Item> averageNumberShipmentsOfEachItemSold(){
+        return null;
 
+    }
+
+    // Topic Purchases, Topic Shipments
+    @RequestMapping(value = "/shop-status", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Item> shopRevenueExpensesProfit(){
+        return null;
+    }
+
+    // Item providing the highest profit
+    @RequestMapping(value = "/highest-profit-item", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Item> averageNumberShipmentsOfEachItemSold(@NotNull @RequestParam Integer minutes){
+        return null; 
+
+    }
+
+    // Item providing the highest profit
+    @RequestMapping(value = "/item-average-sold-price", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Item> averageNumberShipmentsOfEachItemSold(@NotNull @RequestParam String item){
+        return null;
+    }
 }
